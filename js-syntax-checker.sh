b4ead2c9809cf67de531e326604b46ca1f388911
Hendrik <ferox@hotmail.com>
2021-11-16 17:42:45 +0100

    Added debug info

--- js-syntax-checker.sh
+++ js-syntax-checker.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # Check js syntax for changed files with latest commits only.
-
-# always good to know where are we and who are we!
-echo "Who am I ?"
-whoami
-echo "Where am I ?"
-pwd
-
 GIT_PREVIOUS_COMMIT=$1
 GIT_COMMIT=$2
 REPOSITORY_URL=$3
+
+# always good to know where are we and who are we!
+echo "Who am I?"
+whoami
+echo "Where am I?"
+pwd
+echo "Git username?"
+git git config user.name
+
+echo "Repository: $REPOSITORY_URL"
+
 if [ $GIT_PREVIOUS_COMMIT = $GIT_COMMIT ] || [ $GIT_PREVIOUS_COMMIT = "" ]; then
   # let's assume going back to 30 commits would be enough for covering even an exceptional huge PR case.
   GIT_PREVIOUS_COMMIT=$(git rev-list -30 --skip=29 --max-count=1 HEAD)
@@ -18,15 +22,17 @@ fi
 echo "Set ownership to deploy"
 sudo chown deploy:www-data . -R
 
-# stripping https://github.com/
+# Stripping https://github.com/
 REPOSITORY_NAME=${REPOSITORY_URL:19}
+echo "Repository name = $REPOSITORY_NAME"
 git config --unset-all remote.origin.fetch
+echo "Set origin = git@github.com:$REPOSITORY_NAME"
 git remote set-url origin git@github.com:${REPOSITORY_NAME}
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 echo "track all"
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 
-echo "Fetching all"
+echo "Git Fetch --all"
 git fetch --all
 
 echo "All changed files"
